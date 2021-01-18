cc4bebbf378a7d0ef72572dfffe16a6fdc845e01
Taein Kim <kti91@likelion.org>
2021-01-18 23:17:17 +0900

    210118 | added mynweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { authService } from 'fbase';
+import React, { useEffect } from 'react';
+import { authService, dbService } from 'fbase';
 import { useHistory } from 'react-router-dom';
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
 	const history = useHistory();
 	const onLogoutClick = () => {
 		authService.signOut();
 		history.push('/');
 	};
+	const getMyNweets = async () => {
+		const nweets = await dbService
+			.collection('nweets')
+			.where('creatorId', '==', userObj.uid)
+			.orderBy('createdAt', 'desc')
+			.get();
+		console.log(nweets);
+	};
+	useEffect(() => {
+		getMyNweets();
+	});
 	return (
 		<>
 			<button onClick={onLogoutClick}>LogOut</button>
